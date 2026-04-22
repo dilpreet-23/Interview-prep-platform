f5f518db2ef7af2a5ad31310d1f98df5e3b69ef5
dilpreet-23 <2305dilpreet@gmail.com>
2026-04-22 21:58:48 +0530

    first commit

---
+++ frontend/interview-prep-ai/src/components/inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// const ProfilePhotoSelector = (image, setImage) => {
+//   return (
+//     <div>
+//       ProfilePhotoSelector
+//     </div>
+//   )
+// }
+
+// export default ProfilePhotoSelector
